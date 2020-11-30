5a5aa373944c2cc80f552a8397b920d3ae6813ce
efe <adambldwn110@gmail.com>
2020-11-30 21:39:45 +0300

    textleri alma ve listeye ekleme

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
 import {main} from './styles';
 import TodoInput from './components/TodoInput';
 
 const Main = () => {
+
+  const [list, setList] = useState([])
+  
+  function addTodo(text){
+    const element = {
+      id: list.length,
+      todo: text,
+      isDone: false
+    }
+
+    
+    const newArray = [...list, element]
+    setList(newArray)
+    
+  }
+
   return(
     <SafeAreaView style={main.container}>
       <View style={main.container}>
@@ -14,8 +30,13 @@ const Main = () => {
           <Text style={main.todoCount}>10</Text>
         </View>
 
+        <FlatList
+          data={list}
+          renderItem={()=>null}
+        />
+
         <TodoInput 
-          onTodoEnter={todoText => alert(todoText)}
+          onTodoEnter={todoText => addTodo(todoText)}
         />
 
       </View>
